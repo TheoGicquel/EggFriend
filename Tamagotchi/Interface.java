cbae0f7cc3989dc15c311cc6bdd94e64af484086
Rbyaarnrault <50381577+Rbyaarnrault@users.noreply.github.com>
2021-09-08 15:58:38 +0200

    Add files via upload

--- Tamagotchi/Interface.java
+++ Tamagotchi/Interface.java
@@ -1,24 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Décrivez votre classe Interface ici.
- *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
- */
 public class Interface
 {
     public Interface()
     {}
 
     public static void main(String[] args){
+        //Création d'une nouvelle fenêtre
         JFrame ecran = new JFrame("Tamagotchi");
+        
+        //paramètre par default de fermeture du logiciel
         ecran.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ecran.setSize(1000,600);
         ecran.setResizable(false);
         ecran.setBackground(Color.black);
         ecran.setLocationRelativeTo(null);
+        
+        //Création d'un panel et affiliation à la fenêtre
         JPanel panel = new JPanel();
         ecran.setContentPane(panel);
         panel.setMinimumSize(new Dimension(1000,600));
@@ -26,6 +25,7 @@ public class Interface
         panel.setMaximumSize(new Dimension(1000, 600));
         panel.setBackground(Color.gray);
         
+        //affichage graphique de la fenêtre
         ecran.pack();
         ecran.setVisible(true);
         
