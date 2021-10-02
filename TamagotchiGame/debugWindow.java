dc38c76452ceea57a668525330449c368d1fa7d9
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-10-02 15:50:54 +0200

    Maj

---
+++ TamagotchiGame/debugWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class debugWindow extends JFrame {
+
+  // Frame >> panel
+  public debugWindow() {
+    JFrame f = new JFrame("debugWindow");
+    f.setSize(300, 100);
+    f.setVisible(true);
+  }
+}
--- TamagotchiGame/tamagotchiGame.java
+++ TamagotchiGame/tamagotchiGame.java
@@ -1,25 +1,17 @@
-
 import tamagotchiPackage.*;
 import tamagotchiPackage.species.*;
 
-public class tamagotchiGame
-{
-	public static void main(String[] args){
+public class tamagotchiGame {
 
-        tamagotchi gen = new tamagotchi("GEN");
-
-        tamagotchi mouton = new Sheep("monMouton");
-
-        System.out.println("tamagotchi generique : " + gen.getName());
-        System.out.println("espece : " + gen.getBreed());
-
-        System.out.println("\n\n");
-
-
-        System.out.println("tamagotchi mouton : " + mouton.getName());
-        System.out.println("espece : " + mouton.getBreed());
-        
-
-	}
-
+  public static void main(String[] args) {
+    //new debugWindow();
+    new tamagotchiUI();
+    tamagotchi gen = new tamagotchi("GEN");
+    tamagotchi mouton = new Sheep("monMouton");
+    System.out.println("tamagotchi generique : " + gen.getName());
+    System.out.println("espece : " + gen.getBreed());
+    System.out.println("\n\n");
+    System.out.println("tamagotchi mouton : " + mouton.getName());
+    System.out.println("espece : " + mouton.getBreed());
+  }
 }
