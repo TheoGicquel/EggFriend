b3c8c986f0a1c135b9e636f49d789ae1b896a39e
Corentin <corentin.quiniou@outlook.com>
2021-11-10 14:51:35 +0100

    création classe robot

---
+++ TamagotchiGame/tamagotchiPackage/species/Robot.java
@@ -0,0 +1,12 @@
+package tamagotchiPackage.species;
+
+import tamagotchiPackage.tamagotchi;
+
+public class Robot extends tamagotchi {
+    public Robot(String name)
+    {
+        super(name);
+        this.breed="Robot";
+    }
+    
+}
