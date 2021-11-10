a6a89c130f37e43d0f7e131706743c3f12bc05b9
Corentin <corentin.quiniou@outlook.com>
2021-11-10 14:57:46 +0100

    Ajout Chien, Chat

---
+++ TamagotchiGame/tamagotchiPackage/species/Cat.java
@@ -0,0 +1,11 @@
+package tamagotchiPackage.species;
+
+import tamagotchiPackage.tamagotchi;
+
+public class Cat extends tamagotchi{
+    public Cat(String name)
+    {
+        super(name);
+        this.breed="Cat";
+    }
+}
---
+++ TamagotchiGame/tamagotchiPackage/species/Dog.java
@@ -0,0 +1,11 @@
+package tamagotchiPackage.species;
+
+import tamagotchiPackage.tamagotchi;
+
+public class Dog extends tamagotchi{
+    public Dog(String name)
+    {
+        super(name);
+        this.breed="Dog";
+    }
+}
