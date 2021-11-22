9ccd4edda733e77f9fcc321ab7d367ce8e7cd8f9
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-11-22 18:31:35 +0100

    minifix UI

--- src/main/java/com/lado/app/App.java
+++ src/main/java/com/lado/app/App.java
@@ -1,42 +1,35 @@
 package com.lado.app;
 
 import com.lado.app.Tamagotchi.Tamagotchi;
-import com.lado.app.UIPackage.DebugFrame;
 import com.lado.app.UIPackage.*;
+import com.lado.app.UIPackage.DebugFrame;
+
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
+  public App() {}
 
-    public App(){
-       
+  public static void main(String[] args) {
+    DebugMode debug = new DebugMode();
+    Tamagotchi tama = new Tamagotchi();
+    debug = new DebugMode(false);
+    tama = new Tamagotchi();
+    debug.verbose("=== Si vous voyez ceci, vous etes en mode debug !! ===");
+
+    /**
+     * Lancer une fenetre permettant de consulter les infos
+     * sur le jeu et le tamagotchi
+     */
+    if (debug.enabled) {
+      DebugFrame debugWindow = new DebugFrame();
+      debugWindow.loadTama(tama);
     }
 
-    public static void main(String[] args) {
-        DebugMode debug = new DebugMode();
-        Tamagotchi tama = new Tamagotchi();
-        debug = new DebugMode(false);
-        tama = new Tamagotchi();
-        debug.verbose("=== Si vous voyez ceci, vous etes en mode debug !! ===");
+    System.out.print("Hello World!");
 
-        /**
-         * Lancer une fenetre permettant de consulter les infos 
-         * sur le jeu et le tamagotchi 
-         */
-        if(debug.enabled)
-        {
-            DebugFrame debugWindow = new DebugFrame();
-            debugWindow.loadTama(tama);
-
-        }
-
-        System.out.print("Hello World!");
-
-        ContenuFenetre window = new ContenuFenetre();
-
-    }
-
-
+    FenetreGraphique fenetre = new FenetreGraphique();
+    fenetre.setVisible(true);
+  }
 }
