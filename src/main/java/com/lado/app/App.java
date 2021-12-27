b0954384a52044b773356aa32c66f927707832d1
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-12-27 12:10:33 +0100

    cleanup App.java

--- src/main/java/com/lado/app/App.java
+++ src/main/java/com/lado/app/App.java
@@ -1,17 +1,12 @@
 package com.lado.app;
 
 import com.lado.app.Controller.TamagotchiController;
-import com.lado.app.View.UIPackage.GameView.GameView;
 import com.lado.app.View.UIPackage.StartView.StartView;
 
 /**
  * Hello world!
  */
 public class App {
-
-
-  public GameView view;
-  public TamagotchiController controller;
 
 
   public App() {
