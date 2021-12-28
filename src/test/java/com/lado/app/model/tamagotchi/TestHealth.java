1bad80ec774b967b935703fa46833655c1caf959
Théo Gicquel <theo.gicquel.work@gmail.com>
2021-12-28 16:43:30 +0100

    reajout tests avec bon nom de dossier

---
+++ src/test/java/com/lado/app/model/tamagotchi/TestHealth.java
@@ -0,0 +1,52 @@
+package com.lado.app.model.tamagotchi;
+/**
+ * @file HealthTest.java
+ * @author Théo Gicquel
+ * Tests de la vie et de la mort du tamagotchi
+ */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestHealth {
+    @Test
+    public void testisAlive(){
+      Tamagotchi testTamagotchi = new Tamagotchi();
+      assertEquals(true,testTamagotchi.isAlive());
+    }
+    
+    @Test
+    public void testSetAlive(){
+      Tamagotchi testTamagotchi = new Tamagotchi();
+      testTamagotchi.setAlive(false);
+      assertEquals(false, testTamagotchi.isAlive());
+    }
+    
+    @Test
+    public void reviveExpectedFailure()
+    {
+      Tamagotchi testTamagotchi = new Tamagotchi();
+      testTamagotchi.setHealth(0);
+      testTamagotchi.setAlive(false);
+      testTamagotchi.setHealth(100);
+      assertEquals(false, testTamagotchi.isAlive());
+    }
+  
+    @Test
+    public void testSetHealth() {
+      Tamagotchi testTamagotchi = new Tamagotchi();
+  
+      testTamagotchi.setHealth(20);
+      assertEquals(20, testTamagotchi.getHealth());
+    }
+
+
+    @Test
+    public void killTest(){
+      Tamagotchi tama = new Tamagotchi();
+      tama.kill();
+      assertEquals(false,tama.isAlive());
+      assertEquals("Mort",tama.getMood());
+
+    }
+  
+}
